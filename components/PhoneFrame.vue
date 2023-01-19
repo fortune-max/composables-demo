5f6e262f546838c0a54c9593872e38fec0e37b1f
fortune-max <fortmax120@gmail.com>
2023-01-19 12:10:53 +0100

    add PhoneFrame component

---
+++ components/PhoneFrame.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="phone_frame">
+        <img id="pixel_img" src="/pixel_frame.png" />
+        <slot />
+    </div>
+</template>
+
+<style scoped>
+    #pixel_img {
+        height: 900px;
+    }
+
+    #phone_frame {
+        margin-left: 50%;
+        margin-top: 20px;
+        margin-bottom: 20px;
+        display: inline-block;
+        transform: translateX(-50%);
+    }
+</style>
