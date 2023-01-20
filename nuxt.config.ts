d421e83568a53bc96cb9391cbd899cc255f19d32
fortune-max <fortmax120@gmail.com>
2023-01-20 04:19:47 +0100

    add font to head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    app: {
+        head: {
+            link: [
+                { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap' }
+            ]
+        }
+    }
 })
